7c89906b770ea6ca06127eb04fa16b90a31ce51e
EsruDXT <amengceria2017@gmail.com>
2026-03-17 08:05:56 +0700

    wip: implement-homepage

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controllers;
+
+class HomeController
+{
+    public function homeView()
+    {
+        require_once '../app/views/home.php';
+    }
+
+}
+?>
--- public/index.php
+++ public/index.php
@@ -5,6 +5,30 @@ use App\Core\Router;
 
 $router = new Router();
 
+// Register Route
+
+// Login Page
+$router->add('GET', '/login', 'AuthController', 'loginView');
+
+// Register Page
+$router->add('GET', '/register', 'AuthController', 'registerView');
+
+$router->add('GET', '/home', 'HomeController', 'homeView');
+// Manage Product
+// Daftar Produk
+$router->add('GET', '/products', 'ProductController', 'index');
+
+// Tambah
+$router->add('GET', '/products/create', 'ProductController', 'create');
+
+// Edit
+$router->add('GET', '/products/{id}/edit', 'ProductController', 'edit');
+
+// Detail
+$router->add('GET', '/products/{id}', 'ProductController', 'show');
+
+
+
 
 
 $router->run();
